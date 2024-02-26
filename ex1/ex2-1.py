59c7f8f3e3545221bc5c7188f06bd3054736925e
Thomas Viking Christiansson <160872481+ThomasCsson@users.noreply.github.com>
2024-02-26 15:27:38 +0100

    Updates

--- ex1/ex2-1.py
+++ ex1/ex2-1.py
@@ -9,7 +9,11 @@ new_dir.mkdir(exist_ok=True)
 
 print(new_dir.exists())
 
-new_file = new_dir / 'ex_file.py'
+new_file = new_dir / 'ex_file.txt'
 new_file.touch()
 
 print(new_file.exists())
+
+new_file.unlink()
+
+new_dir.rmdir()
